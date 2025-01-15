bbedb6f119162ae45faecaa27904aeaab88cf744
GiovanniAmadei <giovannisebastianoamadei@gmail.com>
2025-01-15 12:18:33 +0100

    Initial Commit

---
+++ SnapTask/SnapTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapTaskApp.swift
+//  SnapTask
+//
+//  Created by giovanni on 15/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SnapTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapTaskTests/SnapTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapTaskTests.swift
+//  SnapTaskTests
+//
+//  Created by giovanni on 15/01/25.
+//
+
+import Testing
+@testable import SnapTask
+
+struct SnapTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
